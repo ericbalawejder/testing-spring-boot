f041a47bfef6212f437cad8c0d42bd227f15ab39
Eric Balawejder <eric.balawejder@gmail.com>
2020-06-11 16:46:05 -0400

    added test deleteById for VetSDJpaService

---
+++ testing-junit5-mockito/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetSDJpaService;
+
+    @Test
+    void deleteById() {
+        vetSDJpaService.deleteById(1L);
+        vetSDJpaService.deleteById(1L);
+
+        verify(vetRepository, times(2)).deleteById(1L);
+    }
+}
