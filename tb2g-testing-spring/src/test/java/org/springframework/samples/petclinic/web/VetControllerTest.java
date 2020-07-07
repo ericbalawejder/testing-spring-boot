c76b27ad8890e8e0390b2fe994c320f3b408700e
Eric Balawejder <eric.balawejder@gmail.com>
2020-07-07 14:45:55 -0400

    added controller tests

--- tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -9,7 +9,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +21,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -31,12 +36,26 @@ class VetControllerTest {
     @InjectMocks
     VetController vetController;
 
-    List<Vet> vetList;
+    List<Vet> vetsList = new ArrayList<>();
+
+    MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        vetList = List.of(new Vet());
-        given(clinicService.findVets()).willReturn(vetList);
+        vetsList.add(new Vet());
+
+        given(clinicService.findVets()).willReturn(vetsList);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vetController).build();
+    }
+
+    @Test
+    void testControllerShowVetList() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+                //.andExpect(status().is3xxRedirection()); // fails
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
     }
 
     @Test
