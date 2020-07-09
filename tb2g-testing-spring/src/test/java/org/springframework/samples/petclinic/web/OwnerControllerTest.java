05fd239c3166a00f4975a6129e2b2bca8a48a586
Eric Balawejder <eric.balawejder@gmail.com>
2020-07-09 10:32:49 -0400

    create tests for processFindFrom in owner controller

--- tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ tb2g-testing-spring/src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -1,12 +1,32 @@
 package org.springframework.samples.petclinic.web;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml",
-        "classpath:spring/mvc-core-config.xml"})
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
     @Autowired
@@ -15,10 +35,65 @@ class OwnerControllerTest {
     @Autowired
     ClinicService clinicService;
 
+    MockMvc mockMvc;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(clinicService);
+    }
 
     @Test
-    void tempTest() {
+    void testProcessFindFormFindByNameNotFound() throws Exception {
+        mockMvc.perform(get("/owners")
+                    .param("lastName", "Dont find ME!"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
 
-        //ownerController
+    @Test
+    void testProcessFindFormReturnListOfOwners() throws Exception {
+        given(clinicService.findOwnerByLastName("")).willReturn(Lists.newArrayList(new Owner(), new Owner()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"));
+
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        assertThat(stringArgumentCaptor.getValue()).isEqualToIgnoringCase("");
+    }
+
+    @Test
+    void testFindOwnerOneResult() throws Exception {
+        Owner owner = new Owner();
+        owner.setId(1);
+        final String findJustOne = "FindJustOne";
+
+        owner.setLastName(findJustOne);
+
+        given(clinicService.findOwnerByLastName(findJustOne)).willReturn(Lists.newArrayList(owner));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName", findJustOne))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        then(clinicService).should().findOwnerByLastName(anyString());
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"));
     }
 }
